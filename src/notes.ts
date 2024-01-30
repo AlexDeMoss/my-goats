386540c50c049583b94652898e09659ab81a0927
Rex Douglas <ademoss@unca.edu>
2024-01-30 13:59:05 -0500

    added notes component

---
+++ src/notes.ts
@@ -0,0 +1,21 @@
+export interface Note {
+    id: number;
+    title: string;
+    text: string;
+}
+
+export const NOTES: Note[] = [
+    {
+        id: 1,
+        title: 'Fainting Goat',
+        text: 'Some goats faint a lot',
+
+
+    },
+    {
+        id: 2,
+        title: 'Shakespaere Goat',
+        text: 'A very dramatic goat',
+
+    },
+];
